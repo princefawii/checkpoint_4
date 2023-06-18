efdc8f0581b0a7f2794968d24d1ec3f50bc483b0
princefawii <keebfawii@yahoo.com>
2023-06-19 00:33:57 +0100

    checkpoint4

---
+++ vat.js
@@ -0,0 +1,11 @@
+let priceOfRice = N900;
+let priceOfYam = N200;
+let priceOfPotato = N300;
+let sum = priceOfRice + priceOfYam + priceOfPotato;
+
+//in every N100, let vat deduction be N2.
+
+
+function addVat(price, vat){
+    
+}
